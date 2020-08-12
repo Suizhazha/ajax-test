dda861a02e4e503ea5140c1d5077fa708f0c36d1
Suizhazha <573505403@qq.com>
2020-08-12 14:51:26 +0800

    完成加载json分页

--- public/main.js
+++ public/main.js
@@ -1,49 +1,92 @@
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest()
+  request.open("GET", "/style.css")//request.status=1
+  request.onreadystatechange = () => {
+    console.log(request.readyState)
 
-getCSS.onclick =()=>{
-  const request  = new XMLHttpRequest()
-  request.open('GET','/style.css')
-  request.onload =()=>{
-    console.log(request.response)
+    if (request.status >= 200 && request.status < 300) {
+      const style = document.createElement("style")
+      style.innerHTML = request.response
+      document.head.append(style)
+    } else console.log("失败了")
+  }
 
-    const style = document.createElement('style')
-    style.innerHTML = request.response
-    document.head.append(style)
-  }
-  request.onerror=()=>{
-    console.log('失败了')
-  }
-  request.send()
+  request.send()//request.status=2
 }
 
-getJS.onclick =()=>{
+getJS.onclick = () => {
   const xhr = new XMLHttpRequest()
-  xhr.open('GET','/main1.js')
-    xhr.onload =()=>{
-      console.log(xhr.response)
-
-      const script =  document.createElement('script')
-      script.innerHTML = xhr.response
-      document.body.appendChild(script)
-
-    }
-    xhr.onerror =()=>{
-
-    }
-    xhr.send()
-}
-
-getHTML.onclick =()=>{
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET','/main.html')
-  xhr.onload =()=>{
+  xhr.open("GET", "/main1.js")
+  xhr.onload = () => {
     console.log(xhr.response)
 
-    const div = document.createElement('div')
-    div.innerHTML = xhr.response
-    document.body.appendChild(div)
+    const script = document.createElement("script")
+    script.innerHTML = xhr.response
+    document.body.appendChild(script)
   }
-  xhr.onerror =()=>{
+  xhr.onerror = () => {
 
   }
   xhr.send()
+}
+
+getHTML.onclick = () => {
+  const xhr = new XMLHttpRequest()
+  xhr.open("GET", "/main.html")
+  xhr.onreadystatechange = () => {
+    console.log(xhr.readyState)
+
+    const div = document.createElement("div")
+    div.innerHTML = xhr.response
+    document.body.appendChild(div)
+  }
+
+  xhr.send()
+}
+
+getXML.onclick = () => {
+  const xhr = new XMLHttpRequest()
+  xhr.open("GET", "/xml1.xml")
+  xhr.onreadystatechange = () => {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+      const dom = xhr.responseXML
+      const text = dom.getElementsByTagName("warning")[0].textContent
+      console.log(text)
+    }
+  }
+
+  xhr.send()
+}
+
+getJSON.onclick = () => {
+  const xhr = new XMLHttpRequest()
+  xhr.open("GET", "/json1.json")
+  xhr.onreadystatechange = () => {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+      let json = JSON.parse(xhr.response)
+      console.log(json)
+      myName.textContent = json.name
+    }
+  }
+  xhr.send()
+}
+
+let n=1
+getNextpage.onclick = () => {
+  const xhr = new XMLHttpRequest()
+  console.log(1)
+  xhr.open("GET", `/page${n+1}`)
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const array = JSON.parse(xhr.response)
+array.forEach(item=>{
+  const li = document.createElement('li')
+  li.textContent = item.id
+  xxx.appendChild(li)
+})
+      n+=1
+    }
+
+  }
+xhr.send()
 }
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
+var http = require("http")
+var fs = require("fs")
+var url = require("url")
 var port = process.argv[2]
 
 if (!port) {
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  console.log("请指定端口号好不啦？\nnode server.js 8888 这样不会吗？")
   process.exit(1)
 }
 
 var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
   var pathWithQuery = request.url
-  var queryString = ''
-  if (pathWithQuery.indexOf('?') >= 0) {
-    queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
+  var queryString = ""
+  if (pathWithQuery.indexOf("?") >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf("?"))
   }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
@@ -21,39 +21,68 @@ var server = http.createServer(function (request, response) {
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log("发请求过来啦！路径（带查询参数）为：" + pathWithQuery)
 
-  if (path === '/index.html') {
+  if (path === "/index.html") {
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(fs.readFileSync('public/index.html'))
-    response.end()
+    response.setHeader("Content-Type", "text/html;charset=utf-8")
 
-  } else if (path === '/main.js') {
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-    response.write(fs.readFileSync('public/main.js'))
-    response.end()
+    let string = fs.readFileSync("public/index.html").toString()//需要变成字符串
 
-  } else if (path === '/style.css') {
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(fs.readFileSync('public/style.css'))
+    //数据库拼接字符串
+    const page1 = fs.readFileSync("db/page1.json")
+    const array = JSON.parse(page1)
+    const result = array.map(item=>`<li>${item.id}</li>`).join(' ')
+    string = string.replace("{{page1}}",`<ul id="xxx">${result}</ul>`)
+
+    response.write(string)
     response.end()
 
-  } else if (path === '/main1.js') {
+  } else if (path === "/main.js") {
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-    response.write(fs.readFileSync('public/main1.js'))
+    response.setHeader("Content-Type", "text/javascript;charset=utf-8")
+    response.write(fs.readFileSync("public/main.js"))
     response.end()
-  }else if (path === '/main.html') {
+
+  } else if (path === "/style.css") {
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(fs.readFileSync('public/main.html'))
+    response.setHeader("Content-Type", "text/css;charset=utf-8")
+    response.write(fs.readFileSync("public/style.css"))
     response.end()
-  }else {
+
+  } else if (path === "/main1.js") {
+    response.statusCode = 200
+    response.setHeader("Content-Type", "text/javascript;charset=utf-8")
+    response.write(fs.readFileSync("public/main1.js"))
+    response.end()
+  } else if (path === "/main.html") {
+    response.statusCode = 200
+    response.setHeader("Content-Type", "text/html;charset=utf-8")
+    response.write(fs.readFileSync("public/main.html"))
+    response.end()
+  } else if (path === "/xml1.xml") {
+    response.statusCode = 200
+    response.setHeader("Content-Type", "text/xml;charset=utf-8")
+    response.write(fs.readFileSync("public/xml1.xml"))
+    response.end()
+  } else if (path === "/json1.json") {
+    response.statusCode = 200
+    response.setHeader("Content-Type", "text/json;charset=utf-8")
+    response.write(fs.readFileSync("public/json1.json"))
+    response.end()
+  } else if (path === "/page2") {
+    response.statusCode = 200
+    response.setHeader("Content-Type", "text/json;charset=utf-8")
+    response.write(fs.readFileSync("db/page2.json"))
+    response.end()
+  } else if (path === "/page3") {
+    response.statusCode = 200
+    response.setHeader("Content-Type", "text/json;charset=utf-8")
+    response.write(fs.readFileSync("db/page3.json"))
+    response.end()
+  } else{
     response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.setHeader("Content-Type", "text/html;charset=utf-8")
     response.write(`你输入的路径不存在对应的内容`)
     response.end()
   }
@@ -62,6 +91,6 @@ var server = http.createServer(function (request, response) {
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请打开 http://localhost:' + port)
+console.log("监听 " + port + " 成功\n请打开 http://localhost:" + port)
 
 
